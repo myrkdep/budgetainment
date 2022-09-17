9ffb7657d82c05a9501b479772395fd4618b98a2
Myrkdep <mardep@LAPTOP-TN4E0328.localdomain>
2022-09-17 18:45:36 +0430

    Started using flask instead of fastapi

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_USERNAME = 'root'
+DATABASE_PASSWORD = 'test'
+DATABASE_NAME = 'test'
+DATABASE_URL = 'db'
+ADMIN_EMAIL = 'myrkdep@gmail.com'
+ADMIN_PASSWORD = 'test'
+SECRET_KEY = 'gfkj@735!$@!%2kjkjgbd'
--- main.py
+++ main.py
@@ -1,55 +1,104 @@
-from fastapi import FastAPI
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_httpauth import HTTPBasicAuth
+from pathlib import Path
+from config import *
 
-app = FastAPI()
+#init app
+app = Flask(__name__)
+database_url = f'mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:3306/{DATABASE_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+auth = HTTPTokenAuth('Bearer')
+token_serializer = Serializer(app.config['SECRET_KEY'], expires_in=3600)
 
-@app.post('/api/signup')
+#databasemodels
+class Member(db.Model):
+    email = db.Column(db.String(200), primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    role = db.Column(db.String(8), nullable=False)
+    active = db.Column(db.SmallInteger, nullable=False)
+
+    def __init__(self, email, name, password):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.role = 'user'
+        self.active = 0
+
+
+#auth setup
+@auth.get_user_roles
+def get_user_roles(email):
+    return Member.query.get(email).type
+
+#routes
+@app.route('/api/health', methods=['GET'])
+def health():
+    return {'msg': 'Ok'}
+
+@app.route('/api/signup', methods=['POST'])
 def signup():
     pass
 
-@app.post('/api/activate')
+@app.route('/api/activate', methods=['POST'])
 def activate():
     pass
 
-@app.post('/api/login')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/login', methods=['POST'])
 def login():
     pass
 
-@app.post('/api/add-sent')
+@auth.login_required(role=['admin', 'user'])
+@app.post('/api/add-sent', methods=['POST'])
 def add_sent():
     pass
 
-@app.post('/api/add-recived')
+@auth.login_required(role=['admin', 'user'])
+@app.post('/api/add-recived', methods=['POST'])
 def add_recived():
     pass
 
-@app.post('/api/add-debt')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/add-debt', methods=['POST'])
 def add_dept():
     pass
 
-@app.delete('/api/delete-debt')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/delete-debt', methods=['DELETE'])
 def delete_debt():
     pass
 
-@app.post('/api/add-demand')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/add-demand', methods=['POST'])
 def add_demand():
     pass
 
-@app.delete('/api/delete-demand')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/delete-demand', methods=['DELETE'])
 def delete_demand():
     pass
 
-@app.get('/api/depts')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/depts', methods=['GET'])
 def depts():
     pass
 
-@app.get('/api/demands')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/demands', methods=['GET'])
 def demands():
     pass
 
-@app.get('/api/report')
+@auth.login_required(role=['admin', 'user'])
+@app.route('/api/report', methods=['GET'])
 def report():
     pass
 
-@app.get('/api/health')
-def health():
-    return {'msg': 'Ok'}
+if __name__ == '__main__':
+    app.run(debug=False)
